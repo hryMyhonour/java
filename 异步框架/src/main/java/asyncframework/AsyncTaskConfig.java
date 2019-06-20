5a20819315bc1bcd3ccfba44ab3dcbcdd92f7173
rongyaohuang <664390603@qq.com>
2019-06-20 20:52:03 +0800

    delete

--- 异步框架/src/main/java/asyncframework/AsyncTaskConfig.java
+++
@@ -1,9 +0,0 @@
-package asyncframework;
-
-import lombok.Data;
-
-@Data
-public class AsyncTaskConfig {
-    public static final int DEFAULT_WAIT_RESPONSE_TIMEOUT_MS = 3 * 60 * 1000;
-    private int waitResponseTimeoutMs = DEFAULT_WAIT_RESPONSE_TIMEOUT_MS;
-}
--- 异步框架/src/main/java/asyncframework/ForceReplacedException.java
+++
@@ -1,7 +0,0 @@
-package asyncframework;
-
-/**
- * 任务被强制替换时报的异常
- */
-public class ForceReplacedException extends RuntimeException {
-}
