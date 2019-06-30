a10480a9d4798c040caba08f524e035083e48914
rongyaohuang <664390603@qq.com>
2019-07-01 00:15:46 +0800

    update

--- springdemo/configserver/src/main/java/configserver/demo/SpringCloudConfigServer.java
+++ springdemo/configserver/src/main/java/configserver/demo/SpringCloudConfigServer.java
@@ -4,25 +4,17 @@
 package configserver.demo;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.config.server.EnableConfigServer;
-
-import java.util.Timer;
-import java.util.TimerTask;
 
 @SpringBootApplication
 @EnableConfigServer
 @Slf4j
-@EnableConfigurationProperties({Config.class})
 public class SpringCloudConfigServer implements CommandLineRunner {
 
-    @Autowired
-    private Config config;
 
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(SpringCloudConfigServer.class);
@@ -32,12 +24,6 @@ public class SpringCloudConfigServer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                log.info("Config value: {}", config);
-            }
-        }, 0, 5000);
+
     }
 }
